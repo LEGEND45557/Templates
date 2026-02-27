80bc4b5f4ba28b2a3bb4e4ca2c2124cf50e155b5
LEGEND45557 <122176966+LEGEND45557@users.noreply.github.com>
2026-02-27 21:11:43 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
-mkdir -p /workspace/runpod-slim/ComfyUI/models/diffusion_models
-mkdir -p /workspace/runpod-slim/ComfyUI/models/loras
-mkdir -p /workspace/runpod-slim/ComfyUI/models/clip
-mkdir -p /workspace/runpod-slim/ComfyUI/models/vae
+mkdir -p /workspace/ComfyUI/models/diffusion_models
+mkdir -p /workspace/ComfyUI/models/loras
+mkdir -p /workspace/ComfyUI/models/clip
+mkdir -p /workspace/ComfyUI/models/vae
 
-wget -nc -P /workspace/runpod-slim/ComfyUI/models/diffusion_models https://huggingface.co/drbaph/Qwen-Image-Edit-2511-FP8/resolve/main/qwen_image_edit_2511_fp8_e4m3fn.safetensors
+wget -nc -P /workspace/ComfyUI/models/diffusion_models https://huggingface.co/drbaph/Qwen-Image-Edit-2511-FP8/resolve/main/qwen_image_edit_2511_fp8_e4m3fn.safetensors
 
-wget -nc -P /workspace/runpod-slim/ComfyUI/models/loras https://huggingface.co/lightx2v/Qwen-Image-Edit-2511-Lightning/resolve/main/Qwen-Image-Edit-2511-Lightning-4steps-V1.0-bf16.safetensors
+wget -nc -P /workspace/ComfyUI/models/loras https://huggingface.co/lightx2v/Qwen-Image-Edit-2511-Lightning/resolve/main/Qwen-Image-Edit-2511-Lightning-4steps-V1.0-bf16.safetensors
 
-wget -nc -P /workspace/runpod-slim/ComfyUI/models/loras https://huggingface.co/Alissonerdx/BFS-Best-Face-Swap/resolve/main/bfs_head_v5_2511_merged_version_rank16_fp16.safetensors
+wget -nc -P /workspace/ComfyUI/models/loras https://huggingface.co/Alissonerdx/BFS-Best-Face-Swap/resolve/main/bfs_head_v5_2511_merged_version_rank16_fp16.safetensors
 
-wget -nc -P /workspace/runpod-slim/ComfyUI/models/clip https://huggingface.co/f5aiteam/CLIP/resolve/main/qwen_2.5_vl_7b_fp8_scaled.safetensors
+wget -nc -P /workspace/ComfyUI/models/clip https://huggingface.co/f5aiteam/CLIP/resolve/main/qwen_2.5_vl_7b_fp8_scaled.safetensors
 
-wget -nc -P /workspace/runpod-slim/ComfyUI/models/vae https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/vae/qwen_image_vae.safetensors
+wget -nc -P /workspace/ComfyUI/models/vae https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/vae/qwen_image_vae.safetensors
